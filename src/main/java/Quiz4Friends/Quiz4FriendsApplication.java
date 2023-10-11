0a3393ffac6fff31352d48f1a9460bdcc7431e95
agug <gutowska93@gmail.com>
2023-10-11 19:58:37 +0200

    Initial commit

---
+++ src/main/java/Quiz4Friends/Quiz4FriendsApplication.java
@@ -0,0 +1,13 @@
+package Quiz4Friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz4FriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz4FriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Quiz4Friends/Quiz4FriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package Quiz4Friends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz4FriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
